04c8353608773e00b95af4c4be4fea375941b83f
iswaryamantina <iswarya252897@gmail.com>
2023-01-13 09:16:30 +0530

    assignment completed.

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 function convertToRoman(num) {
   	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+      1: 'I',
+	  4: 'IV',
+	  5: 'V',
+	  9: 'IX',
+	  10: 'X',
+	  40: 'XL',
+	  50: 'L',
+	  90: 'XC',
+	  100: 'C',
+	  400: 'CD',
+	  500: 'D',
+	  900: 'CM',
+	  1000: 'M',
     };
 
   //your code here
+	let romanized = "";
+	const decimalKeys = Object.keys(obj).reverse();
 
+	decimalKeys.forEach(key=> {
+		while(key <= num) {
+			romanized += obj[key];
+			num -= key;
+		}
+	})
+	return romanized;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
